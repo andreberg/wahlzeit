6ea1c18a9482f91f8a041aff3d0a44bf9b643974
André Berg <andre@irisvfx.com>
2022-12-04 20:10:55 +0100

    Add DbC to CGIPhoto.

--- src/main/java/org/wahlzeit/model/CGIPhoto.java
+++ src/main/java/org/wahlzeit/model/CGIPhoto.java
@@ -6,9 +6,17 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import static org.wahlzeit.utils.AssertUtil.*;
+
 public class CGIPhoto extends Photo {
 
     public static final String SOFTWARE = "software";
+
+    /**
+     * This string is shown in place of the software name so the website user knows
+     * the software name entered previously could not be fetched from DB.
+     */
+    public static final String ERR_MSG_READING_SOFTWARE_NAME_FROM_DB = "reading software name from database failed";
 
     protected String softwareName = "";
 
@@ -32,7 +40,60 @@ public class CGIPhoto extends Photo {
         softwareName = rset.getString("software_name");
     }
 
+    /**
+     * @param photo
+     * @param softwareName
+     * @methodtype constructor
+     */
+    public CGIPhoto(Photo photo, String softwareName) {
+        copyPhotoFields(photo);
+        this.softwareName = softwareName;
+    }
+
+    /**
+     * @Preconditions: photo != null
+     * @Postconditions:
+     * @Invariants: this.softwareName != null
+     *
+     * @param photo
+     * @methodtype constructor
+     */
+    public CGIPhoto(Photo photo) {
+
+        assertNotNull(photo);
+
+        copyPhotoFields(photo);
+        softwareName = readSoftwareNameFromDBForID(photo.getId().asInt());
+
+        assertClassInvariants();
+    }
+
+    // --- Getters/Setters ---
+
+    public String getSoftwareName() {
+        return softwareName;
+    }
+
+    /**
+     * @Preconditions: softwareName != null
+     * @Postconditions:
+     * @Invariants: this.softwareName != null
+     */
+    public void setSoftwareName(String softwareName) {
+        this.softwareName = softwareName;
+        assertClassInvariants();
+    }
+
+    // --- Helper Methods ---
+
+    /**
+     * @Preconditions: photo != null
+     * @Postconditions: this.id == photo.id
+     * @Invariants: this.softwareName != null
+     */
     protected void copyPhotoFields(Photo photo) {
+
+        assertNotNull(photo);
 
         id = photo.id;
         ownerId = photo.ownerId;
@@ -50,14 +111,25 @@ public class CGIPhoto extends Photo {
         praiseSum = photo.praiseSum;
         noVotes = photo.noVotes;
         creationTime = photo.creationTime;
+
+        // this.id == photo.id is trivially valid
+
+        assertClassInvariants();
     }
 
-    public CGIPhoto(Photo photo, String softwareName) {
-        copyPhotoFields(photo);
-        this.softwareName = softwareName;
-    }
+    // --- Query Methods ---
 
+    /**
+     * @Preconditions: id >= 0
+     * @Postconditions: result != null && result.length() > 0
+     * @Invariants: this.softwareName != null
+     */
     protected String readSoftwareNameFromDBForID(int id) {
+
+        // preconditions
+        assertNonNegative(id);
+
+        String result = ERR_MSG_READING_SOFTWARE_NAME_FROM_DB;
         try {
             PreparedStatement stmt = CGIPhotoManager.getMyInstance().exposeReadingStatement("SELECT * FROM photos WHERE id = ?");
             stmt.setInt(1, id);
@@ -65,39 +137,74 @@ public class CGIPhoto extends Photo {
             if (rset.next()) {
                 softwareName = rset.getString("software_name");
             }
-            return softwareName;
+            result = softwareName;
         } catch (SQLException sex) {
             SysLog.logThrowable(sex);
         }
-        return null;
+
+        // postconditions
+        assertStringNotNullOrEmpty(result);
+
+        assertClassInvariants();
+
+        return result;
     }
 
-    public CGIPhoto(Photo photo) {
-        copyPhotoFields(photo);
-        softwareName = readSoftwareNameFromDBForID(photo.getId().asInt());
-    }
+    // --- Photo Overrides ---
 
+    /**
+     * @Preconditions: cfg != null
+     * @Postconditions: result != 0 && result.length() > 0
+     * @Invariants: this.softwareName != null
+     */
     @Override
     public String getSummary(ModelConfig cfg) {
-        return cfg.asCGIPhotoSummary(ownerName, softwareName);
+
+        // preconditions
+        assertNotNull(cfg);
+
+        String result = cfg.asCGIPhotoSummary(ownerName, softwareName);
+
+        // postconditions
+        assertStringNotNullOrEmpty(result);
+
+        assertClassInvariants();
+        return result;
     }
 
-    public String getSoftwareName() {
-        return softwareName;
-    }
-
-    public void setSoftwareName(String softwareName) {
-        this.softwareName = softwareName;
-    }
-
+    /**
+     * @Preconditions: rset != null
+     * @Postconditions:  this.softwareName.length() > 0
+     * @Invariants: this.softwareName != null
+     */
     public void readFrom(ResultSet rset) throws SQLException {
+
+        assertNotNull(rset);
+
         super.readFrom(rset);
         softwareName = rset.getString("software_name");
+
+        assertGreater(softwareName.length(), 0);
+
+        assertClassInvariants();
     }
 
+    /**
+     * @Preconditions: rset != null
+     * @Postconditions:
+     * @Invariants: this.softwareName != null
+     */
     public void writeOn(ResultSet rset) throws SQLException {
+
+        assertNotNull(rset);
+
         super.writeOn(rset);
         rset.updateString("software_name", softwareName);
+
+        assertClassInvariants();
     }
 
+    public void assertClassInvariants() {
+        assert this.softwareName != null : "Design-by-contract violation: softwareName can't be null";
+    }
 }
