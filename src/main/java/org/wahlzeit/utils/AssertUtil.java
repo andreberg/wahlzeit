08b73a264f0b248a7c0873a2bc8f358c395f01fc
André Berg <andre@irisvfx.com>
2022-12-04 20:10:01 +0100

    Add more assertion methods to AssertUtil.

--- src/main/java/org/wahlzeit/utils/AssertUtil.java
+++ src/main/java/org/wahlzeit/utils/AssertUtil.java
@@ -16,11 +16,31 @@ public class AssertUtil {
         assert o != null;
     }
 
+    static public void assertNull(Object o) {
+        assert o == null;
+    }
+
     static public void assertIsFinite(Double value) {
         assert Double.isFinite(value);
     }
 
     static public void assertNonNegative(Double value) {
         assert Double.compare(value, 0) >= 0;
+    }
+
+    static public void assertNonNegative(int value) {
+        assert value >= 0;
+    }
+
+    static public void assertPlusOne(int value, int oldValue) {
+        assert value == oldValue + 1;
+    }
+
+    static public void assertGreater(int a, int b) {
+        assert a > b;
+    }
+
+    static public void assertStringNotNullOrEmpty(String s) {
+        assert !StringUtil.isNullOrEmptyString(s);
     }
 }
